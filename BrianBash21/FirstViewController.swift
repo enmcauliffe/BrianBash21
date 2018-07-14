04aff85125aa4a8b9094e25b4f411a7442f24a62
JalIrani <jaljasonirani@gmail.com>
2018-07-14 14:42:24 -0400

    countdown done

--- BrianBash21/FirstViewController.swift
+++ BrianBash21/FirstViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var daysLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
+    @IBOutlet weak var minutesLabel: UILabel!
+    @IBOutlet weak var secondsLabel: UILabel!
+    
+    var timer = Timer()
+    var isTimerRunning = false
+    var seconds = 60
+    let calendar = Calendar.current
+    
+    var bBday:Date?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // here we set the current date
+        
+//        let date = NSDate()
+//
+//        let components = calendar.dateComponents([.hour, .minute, .month, .year, .day], from: date as Date)
+//
+//        let currentDate = calendar.date(from: components)
+        
+        let userCalendar = Calendar.current
+        
+        // here we set the due date. When the timer is supposed to finish
+        let competitionDate = NSDateComponents()
+        competitionDate.year = 2019
+        competitionDate.month = 2
+        competitionDate.day = 18
+        competitionDate.hour = 00
+        competitionDate.minute = 00
+        bBday = userCalendar.date(from: competitionDate as DateComponents)!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(FirstViewController.updateTimer)), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +52,28 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @objc func updateTimer() {
+        let date = NSDate()
+        
+        let components = calendar.dateComponents([.hour, .minute, .month, .year, .day, .second], from: date as Date)
+        
+        let currentDate = calendar.date(from: components)
+        let sevenDaysAgo = Calendar.current.date(byAdding: .second, value: -1, to: bBday!)
+        
+        let CompetitionDayDifference = calendar.dateComponents([.day, .hour, .minute, .second], from: currentDate!, to: sevenDaysAgo!)
+        
+        //finally, here we set the variable to our remaining time
+        let daysLeft = CompetitionDayDifference.day!
+        let hoursLeft = CompetitionDayDifference.hour!
+        let minutesLeft = CompetitionDayDifference.minute!
+        let secondsLeft = CompetitionDayDifference.second!
+        
+        daysLabel.text = "\(String(describing: daysLeft)) DAYS"
+        hoursLabel.text = "\(String(describing: hoursLeft)) HOURS"
+        minutesLabel.text = "\(String(describing: minutesLeft)) MINUTES"
+        secondsLabel.text = "\(String(describing: secondsLeft)) SECONDS"
+        
+    }
+    
 }
 
